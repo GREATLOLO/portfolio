8fab45a509f2a87a8adb7b4c4043c8b81a0359ce
Kevin Li <49249492+GREATLOLO@users.noreply.github.com>
2025-01-25 15:55:52 -0800

    let

--- global.js
+++ global.js
@@ -3,3 +3,12 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+
+let currentLink = navLinks.find(
+    (a) => a.host === location.host && a.pathname === location.pathname
+ );
+
+ if (currentLink) {
+    // or if (currentLink !== undefined)
+    currentLink.classList.add('current');
+}
