1e2bf21f7d9d38f6d2586435b05365b06b3fee95
Kevin Li <49249492+GREATLOLO@users.noreply.github.com>
2025-01-25 20:23:43 -0800

    finally!!

--- global.js
+++ global.js
@@ -4,11 +4,83 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
-let currentLink = navLinks.find(
-    (a) => a.host === location.host && a.pathname === location.pathname
- );
+// let currentLink = navLinks.find(
+//     (a) => a.host === location.host && a.pathname === location.pathname
+//  );
 
- if (currentLink) {
-    // or if (currentLink !== undefined)
-    currentLink.classList.add('current');
+//  if (currentLink) {
+//     // or if (currentLink !== undefined)
+//     currentLink.classList.add('current');
+// }
+
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    {url: 'resume/', title: 'Resume'},
+    {url: 'contact/', title: 'Contact'},
+    {url: 'https://github.com/GREATLOLO', title: 'Me', external:true}
+  ];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+const ARE_WE_HOME = document.documentElement.classList.contains('home');
+
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+    // TODO create link and add it to nav
+    url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    nav.append(a);
+    a.classList.toggle(
+        'current',
+        a.host === location.host && a.pathname === location.pathname
+      );
+ }
+
+document.body.insertAdjacentHTML(
+    'afterbegin',
+    `
+    <label class="color-scheme">
+        Theme:
+        <select>
+            <option value="auto">Automatic</option>
+            <option value="light">Light</option>
+            <option value="dark">Dark</option>
+        </select>
+    </label>`
+);
+
+const select = document.querySelector('select');
+
+if(localStorage.colorScheme){
+    console.log('Color scheme changed to', localStorage.colorScheme);
+    document.documentElement.style.setProperty('color-scheme', localStorage.colorScheme);
+    select.value = localStorage.colorScheme
 }
+
+select.addEventListener('input', function (event) {
+  console.log('Color scheme changed to', event.target.value);
+  document.documentElement.style.setProperty('color-scheme', event.target.value);
+  localStorage.colorScheme = event.target.value
+});
+
+const form = document.querySelector('form');
+
+form?.addEventListener('submit', function (event) {
+    event.preventDefault();
+    const data = new FormData(form);
+    let link = 'mailto:kel062@ucsd.edu?'
+    for (let [name, value] of data) {
+        link += `${encodeURIComponent(name)}=${encodeURIComponent(value)}&`;
+        console.log(link);
+        location.href = link;
+      }
+
+});
+
+
+  
