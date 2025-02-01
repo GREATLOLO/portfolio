1cfc9e20ab7e7ca84abd7ae87b416f0855400f0c
Kevin Li <49249492+GREATLOLO@users.noreply.github.com>
2025-01-31 16:19:04 -0800

    fia

--- global.js
+++ global.js
@@ -129,3 +129,18 @@ export async function fetchGithubData(username) {
   return fetchJSON(`https://api.github.com/users/${username}`);
 }
 
+const githubData = await fetchGithubData('GREATLOLO');
+const profileStats = document.querySelector('#profile-stats');
+
+if (profileStats) {
+    profileStats.innerHTML = `
+          <h2>My Github Stats</h2>
+          <dl class = "status" id = "status">
+            <dt>Public Repos</dt><dd>${githubData.public_repos}</dd>
+            <dt>Public Gists</dt><dd>${githubData.public_gists}</dd>
+            <dt>Followers</dt><dd>${githubData.followers}</dd>
+            <dt>Following</dt><dd>${githubData.following}</dd>
+          </dl>
+      `;
+  }
+
